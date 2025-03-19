f74f294f796c59d1e61bae70553a0dc3b74c8abc
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 17:29:16 +0000

    add request_response_protocol (cargo) benchmark result for 954d8571df26b5cc0d342c6c05d414bd6912f1cd

--- bench/request_response_protocol/data.js
+++ bench/request_response_protocol/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742384119436,
+  "lastUpdate": 1742405355775,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "request_response_protocol": [
@@ -25487,6 +25487,114 @@ window.BENCHMARK_DATA = {
             "name": "request_response_protocol/litep2p/serially/16MB",
             "value": 2605207674,
             "range": "± 52389809",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "1728078+michalkucharczyk@users.noreply.github.com",
+            "name": "Michal Kucharczyk",
+            "username": "michalkucharczyk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "954d8571df26b5cc0d342c6c05d414bd6912f1cd",
+          "message": "`fatxpool`: report_invalid: do not ban Future/Stale txs from re-entering the view (#7777)\n\n#### Description\n\nAvoid banning future/stale transactions reported as invalid by the\nauthorship module.\n\n#### Note for reviewers\nWhen re-org is handled by transaction pool, the view for new fork\n(`Bn'`) is cloned from the tip of the other existing fork (`Bn`). The\nnew view is not entirely re-validated during the maintain process (it\nwill be revalidated in the background), so it may happen that it\ncontains transactions that are ready on (`Bn`) but actually are not\nready on (`Bn'`). All required (which are expected to be in retracted\nset) transactions are submitted to the new view, but order of txs in\nready iterator is not updated.\n\nThe proper fix would require to re-build the the iterator - which is not\ntrivial as we do not have tags for transactions for block `Bn'` yet. We\ncould force retracted txs to be before ready transactions but it also\ndoes not feel to be a good solution - it still would be best effort\ntrial.\n\nFor now allowing future transactions to re-enter the view immediately is\nin my opinion a good compromise. This PR is a quick fix and actually\nbrings back behavior of txpool from before merging #6008. The bad thing\nis that incorrect transactions are detected during block authorship, but\nthis situation to happen requires some specific pre-conditions which\nshould be rare.\n\nIf this PR is not merged, some transaction will get included into\nblocks, only after\n[`DEFAULT_BAN_TIME_SECS`](https://github.com/paritytech/polkadot-sdk/blob/4b39ff00b887039bc3e02a763a29deb09df56833/substrate/client/transaction-pool/src/graph/rotator.rs#L37),\nwhich is pretty bad.\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-03-19T16:20:25Z",
+          "tree_id": "1de422e3985f413c3a4cd2e3600fb1cf6ffe5e55",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/954d8571df26b5cc0d342c6c05d414bd6912f1cd"
+        },
+        "date": 1742405338616,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "request_response_protocol/libp2p/serially/64B",
+            "value": 17743458,
+            "range": "± 136817",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/512B",
+            "value": 18103113,
+            "range": "± 120621",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/4KB",
+            "value": 19752011,
+            "range": "± 317082",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/64KB",
+            "value": 23695894,
+            "range": "± 310232",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/256KB",
+            "value": 52745912,
+            "range": "± 699352",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/2MB",
+            "value": 341800089,
+            "range": "± 6221839",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/libp2p/serially/16MB",
+            "value": 2638018405,
+            "range": "± 217123090",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/64B",
+            "value": 14697197,
+            "range": "± 320788",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/512B",
+            "value": 14721548,
+            "range": "± 112426",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/4KB",
+            "value": 15332329,
+            "range": "± 165748",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/64KB",
+            "value": 19581665,
+            "range": "± 155417",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/256KB",
+            "value": 49800067,
+            "range": "± 750776",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/2MB",
+            "value": 292034951,
+            "range": "± 2590217",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "request_response_protocol/litep2p/serially/16MB",
+            "value": 2350609351,
+            "range": "± 34383588",
             "unit": "ns/iter"
           }
         ]
